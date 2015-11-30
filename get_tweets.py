cc7926c4e8ab96e4a2a86b95f8b9d7a38e748698
craigbradford <bradford.craig87@gmail.com>
2015-11-30 11:52:05 +0000

    Update comments

--- get_tweets.py
+++ get_tweets.py
@@ -1,13 +1,10 @@
 import re
 import csv
 import datetime
-
 import requests
 import tweepy
-
 from xml.etree import ElementTree
 from collections import Counter
-
 from tweepy import API
 from tweepy import OAuthHandler
 from tld import get_tld
@@ -18,7 +15,7 @@ from api_keys import *
 # Twitter auth
 auth = tweepy.OAuthHandler(twitter_ckey, twitter_csecret)
 auth.set_access_token(twitter_atoken, twitter_asecret)
-api = tweepy.API(auth)  
+api = tweepy.API(auth)
 
 # create some empty lists
 links = []
@@ -27,11 +24,14 @@ firstpass = []
 topics = []
 tags = []
 
-# make variables for the two Alchemi APIs I want to use
+# make variables for the two Alchemy APIs I want to use
 ConceptsAPI = "http://gateway-a.watsonplatform.net/calls/url/URLGetRankedConcepts?apikey=" + watson_api_key + "&url="
 
-usernames = open('usernames.txt', 'r+') # make a variable for the Twitter usernames file by reading the text file usernames.txt
-usernames = usernames.read().splitlines() # create a variable named usernames which is the usernames split by line. 
+# make a variable for the Twitter usernames file by reading the text
+# file usernames.txt
+usernames = open('usernames.txt', 'r+')
+# create a variable named usernames which is the usernames split by line.
+usernames = usernames.read().splitlines()
 
 
 def CountDomains(usernames):
@@ -39,39 +39,56 @@ def CountDomains(usernames):
     print "Starting tweet collection..."
     print str(len(usernames)) + " usernames.\n"
 
-    for name in usernames: #for all the usernames in the usernames file...
+    # for all the usernames in the usernames file...
+    for name in usernames:
         try:
-            public_tweets = api.user_timeline(name, count=20) #create a variable named public_tweets which is the users last n tweets
+            # create a variable named public_tweets
+            public_tweets = api.user_timeline(name, count=20)
         except:
             print "private profile"
-            continue 
-        for tweet in public_tweets: #for every tweet in the public_tweets list...
-        
-            urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', tweet.text) # use regex to find the links in the tweet body
-            
-            for url in urls: # for every url in the list of urls that the regex found
+            continue
+        # for every tweet in the public_tweets list...
+        for tweet in public_tweets:
+
+            # use regex to find the links in the tweet body
+            urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', tweet.text)
+
+            # for every url in the list of urls that the regex found
+            for url in urls:
                 try:
-                    link = requests.get(url, allow_redirects=True).url  # request each link using the requests library and follow redirects to the end. This stops links like t.co showing up in the results
-                    links.append(link)# put all of the urls into the list named links
-                    domain = get_tld(link)#use the get_tld function from the tld library to turn urls into domains
-                    domains.append(domain)#append the domain to the domains list that was created at the start
-                    
+                    # request each link using the requests library
+                    # follow redirects.
+                    link = requests.get(url, allow_redirects=True).url
+                    # put all of the urls into the list named links
+                    links.append(link)
+                    # use the get_tld function from the tld library
+                    domain = get_tld(link)
+                    # append the domain to the domains list
+                    domains.append(domain)
                 except:
                     pass
 
         print "\tFinished: " + name
-    
+
     print "\nStarting Alchemy analysis of links..."
     print str(len(links)) + " links.\n"
 
-    for link in links: # for each of the urls in the urls list that was created above...
-        apiurl = ConceptsAPI+link # create a new url by concatenating the concepts API base url (defined at the start) to the link we want to get the concepts for
-        
-        r =  requests.get(apiurl) # create a variable named r which is the content from the API request
+    # for each of the urls in the urls list that was created above...
+    for link in links:
+        # create a new url by concatenating the concepts API base url
+        # (defined at the start) to the link we want to get the concepts for
+        apiurl = ConceptsAPI+link
 
-        doc = ElementTree.fromstring(r.text) # use the ElementTree function from the xml library to create a new variable named doc from the xml returned in the API call
-        
-        for tag in doc.findall('.//text'): # find all the <text> tags in the xml that is returned and assign the name tag. The <text> tag is the concept text that we want to keep a list of.
+        # create a variable named r which is the content from the API request
+        r = requests.get(apiurl)
+
+        # use the ElementTree function from the xml library to create
+        # a new variable named doc from the xml returned in the API call
+        doc = ElementTree.fromstring(r.text)
+
+        # find all the <text> tags in the xml and assign the name tag.
+        # The <text> tag is the concept text that we want to keep a list of.
+        for tag in doc.findall('.//text'):
             tags.append(tag.text)
         print "\tFinished: " + link
 
@@ -91,8 +108,5 @@ def CountDomains(usernames):
         for tag, count in dict(Counter(tags)).items():
             conceptswriter.writerow([tag, count])
         concepts.close()
-        
 
 CountDomains(usernames)
-    
-      
